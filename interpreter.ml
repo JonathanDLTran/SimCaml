4cf520b76f0ed9640bc360766b1e7a1c55bb978c
Jonathan Tran <jtran2718@gmail.com>
2019-11-07 02:31:54 -0500

    Added free variables trackign for match statements, todo finish anonymous functions and application

--- interpreter.ml
+++ interpreter.ml
@@ -1,9 +1,5 @@
 type bop = Add | Sub | Mult | Div | And | Or | GT | LT | GEQ | LEQ | EQ
 
-type tuple_direction = 
-  | Left
-  | Right
-
 type expr = 
   | Int of int
   | Bool of bool
@@ -16,34 +12,63 @@ type expr =
   | Let of expr * expr * expr
   | Left of expr
   | Right of expr
+  | MatchWith of expr * expr * expr * expr * expr
+  | Fun of string * expr
+  | Apply of expr * expr
 
 (* TODO : Match statements on variant left/right *)
 (* TODO : Anonymous functions *)
 
 let rec is_value expr = 
   match expr with
-  | Int _ | Bool _ -> true
+  | Int _ | Bool _  | Fun _ -> true
   | Tuple (l, r) as t -> is_tuple_value t
   | Left e  | Right e -> is_value e
-  | Binop _ | IfThenElse _ | Var _ | Let _ | Fst _ | Snd _ -> false
+  | Binop _ | IfThenElse _ | Var _ | Let _ | Fst _ | Snd _  | MatchWith _ | Apply _-> false
 
 and is_tuple_value tuple = 
   match tuple with
   | Tuple (e1 , e2) -> 
     if is_value e1 && is_value e2 then true
     else false
-  | _ -> failwith "Precondition violated: Must be tuple"
+  | _ -> failwith "Precondition Violation : Must be tuple"
 
 let is_var v = 
   match v with
   | Var _ -> true
   | _ -> false
 
-let get_var v = 
+let get_var_name v = 
   match v with
-  | Var str -> str
-  | _ -> failwith "Precondition violated: v must be a variable"
+  | Var s -> s
+  | _ -> failwith "v was not a variable."
 
+let rec free_variables expr = 
+  match expr with
+  | Int i -> []
+  | Bool b -> []
+  | Var x -> [x]
+  | Binop (binop, e1, e2) ->
+    free_variables e1 @ free_variables e2
+  | Tuple (e1, e2) ->
+    free_variables e1 @ free_variables e2
+  | Fst e1 -> free_variables e1
+  | Snd e1 -> free_variables e1
+  | Left e1 -> free_variables e1
+  | Right e1 -> free_variables e1
+  | MatchWith (e1, s1, e2, s2, e3) ->
+    free_variables e1 @ 
+    List.filter (fun elt -> elt <> get_var_name s1) (free_variables e2) @
+    List.filter (fun elt -> elt <> get_var_name s2) (free_variables e3)
+  | Apply (e1, e2) -> 
+    free_variables e1 @ free_variables e2
+  | Fun (x, e) -> 
+    List.filter (fun elt -> elt <> x) (free_variables e)
+  | IfThenElse (e1, e2, e3) ->
+    free_variables e1 @ free_variables e2 @ free_variables e3
+  | Let (var, e1, e2) ->
+    free_variables e1 @ 
+    List.filter (fun elt -> elt <> get_var_name var) (free_variables e2)
 
 (* substitution pattern should follow the basic rules: 
    let x = e1 in e2
@@ -69,9 +94,9 @@ let get_var v =
 let rec substitute v x e = 
   match e with
   | Int _ | Bool _ -> failwith "e must have a variable binding in it. "
-  | Var name -> 
-    if name = x then v 
-    else Var name (* This case is probably not right *)
+  | Var name as n -> 
+    if n = x then v 
+    else n (* This case is probably not right *)
   | Binop (bop, e1, e2) -> 
     Binop (bop, substitute v x e1, substitute v x e2)
   | Tuple (e1, e2) ->
@@ -82,21 +107,36 @@ let rec substitute v x e =
     IfThenElse (substitute v x e1, substitute v x e2, substitute v x e3)
   | Let (var, e1, e2) ->
     if is_var var then 
-      let var_name = get_var v in 
-      if var_name = x then Let (var, substitute v x e1, e2) (* shadowing case *)
+      if var = x then Let (var, substitute v x e1, e2)
       else Let (var, substitute v x e1, substitute v x e2)
-    else failwith "Var is not a variable and so substition cannot occur"
+    else failwith "Precondition Violation : var must be a variable."
   | Left e1 -> Left (substitute v x e1)
   | Right e1 -> Right (substitute v x e1)
+  | MatchWith (e1, s1, e2, s2, e3) ->
+    let var_set = [get_var_name s1] @ [get_var_name s2] in 
+    let free_vars = free_variables v in
+    if List.for_all (fun elt -> List.mem elt free_vars) var_set then MatchWith (substitute v x e1, s1, substitute v x e2, s2, substitute v x e3)
+    else if List.mem (get_var_name s2) free_vars then MatchWith (substitute v x e1, s1, e2, s2, substitute v x e3)
+    else if List.mem (get_var_name s1) free_vars then MatchWith (substitute v x e1, s1, substitute v x e2, s2, e3)
+    else MatchWith (substitute v x e1, s1, e2, s2, e3)
+  | Fun (var, e) ->
+    failwith "Unimplemented"
+  | Apply (e1, e2) ->
+    failwith "Unimplemented"
 
-let same_type e1 e2 = 
+(* same type can be relagated to a type checker to do instead *)
+(*
+let rec same_type e1 e2 = 
   match e1, e2 with
   | Int i1, Int i2 -> true
   | Bool b1, Bool b2 -> true
   | Tuple (l1, r1), Tuple (l2, r2) -> true
-  | Left v1, Left v2 -> true
-  | Right v1, Right v2 -> true
+  | Left v1, Left v2 -> same_type v1 v2
+  | Right v1, Right v2 -> same_type v1 v2
+  | Right v1, Left v2 -> same_type v1 v2
+  | Left v1, Right v2 -> same_type v1 v2
   | _ -> false
+*)
 
 (** Requires: [expr] is not a value. *)
 let rec step expr = 
@@ -143,6 +183,14 @@ let rec step expr =
   | Right e1 when is_value e1 ->
     failwith "Precondition violated: cannot step value"
   | Right e1 -> Right (step e1)
+  | MatchWith (e1, s1, e2, s2, e3) when not (is_value e1) ->
+    MatchWith (step e1, s1, e2, s2, e3)
+  | MatchWith (e1, s1, e2, s2, e3) ->
+    step_match_with e1 s1 e2 s2 e3
+  | Fun (var, e) ->
+    failwith "Unimplemented"
+  | Apply (e1, e2) ->
+    failwith "Unimplemented"
 
 and step_bop bop e1 e2 = 
   match bop, e1, e2 with
@@ -164,29 +212,42 @@ and step_bop bop e1 e2 =
 
 and step_if_then_else e1 e2 e3 = 
   match e1 with
-  | Bool true -> 
+  | Bool true -> e2
+  (*
     if same_type e2 e3 then e2 
-    else failwith "If statement branches must have same type"
-  | Bool false ->
+    else failwith "If statement branches must have same type" *)
+  (* I push back type checking into its own module, to parse the ast before interpreation*)
+  | Bool false -> e3
+    (*
     if same_type e2 e3 then e3
-    else failwith "If statement branches must have same type"
-  | Int _ | Binop _ | Tuple _ | Left _ | Right _-> failwith "Guard of if statement must be boolean"
+      else failwith "If statement branches must have same type"*)
+  (* I push back type checking into its own module, to parse the ast before interpreation*)
+  | Int _ | Binop _ | Tuple _ | Left _ | Right _ | Fun _ -> 
+    failwith "Type Error : Guard of if statement must be boolean"
   | Var s -> failwith ("Unbound variable " ^ s)
-  | IfThenElse _ | Let _ | Fst _ | Snd _  -> failwith "Precondition Violation: e1 must be a value"
+  | IfThenElse _ | Let _ | Fst _ | Snd _  | MatchWith _ | Apply _ ->
+    failwith "Precondition Violation : e1 must be a value"
 
 and step_let e1 e2 e3 = 
-  if is_var e1 then substitute e2 (get_var e1) e3
-  else failwith "Var is not a variable and so substition cannot occur"
+  if is_var e1 then substitute e2 e1 e3
+  else failwith "Precondition Violation : e1 must be a variable."
 
 and step_fst e1 = 
   match e1 with
   | Tuple (l, r) -> l
-  | _ -> failwith "Precondition violated: expression must a tuple value"
+  | _ -> failwith "Precondition Violation: expression must a tuple value"
 
 and step_snd e1 = 
   match e1 with
   | Tuple (l, r) -> r
-  | _ -> failwith "Precondition violated: expression must a tuple value"
+  | _ -> failwith "Precondition Violation: expression must a tuple value"
+
+and step_match_with e1 s1 e2 s2 e3 = 
+  match e1 with
+  | Left v -> substitute v s1 e2
+  | Right v -> substitute v s2 e3
+  | _ -> 
+    failwith "Match error: Match with can only match on left and right constructors. "
 
 let rec eval expr = 
   if is_value expr then expr 
@@ -202,42 +263,16 @@ let parse s =
   ast
   *)
 
-let rec string_of_tuple t = 
-  match t with
-  | Int _ | Bool _ | Binop _ | IfThenElse _ | Let _ | Fst _ | Snd _ | Right _  | Left _->
-    failwith "Precondition violated: t must be a tuple. "
-  | Var s -> failwith ("Unbound variable " ^ s)
-  | Tuple (l, r) -> 
-    match l, r with 
-    | Int i1, Int i2 -> 
-      "(" ^ string_of_int i1 ^ " , " ^ string_of_int i2 ^ ")"
-    | Int i, Bool b ->
-      "(" ^ string_of_int i ^ " , " ^ string_of_bool b ^ ")"
-    | Bool b, Int i ->
-      "(" ^ string_of_bool b ^ " , " ^ string_of_int i ^ ")"
-    | Bool b1, Bool b2 ->
-      "(" ^ string_of_bool b1 ^ " , " ^ string_of_bool b2 ^ ")"
-    | Int i, Tuple (li, ri) ->
-      "(" ^ string_of_int i ^ " , " ^ string_of_tuple (Tuple (li, ri))  ^ ")"
-    | Tuple (li, ri), Int i ->
-      "(" ^ string_of_tuple (Tuple (li, ri))  ^ " , " ^ string_of_int i ^ ")"
-    | Bool b, Tuple (li, ri) ->
-      "(" ^ string_of_bool b ^ " , " ^ string_of_tuple (Tuple (li, ri))  ^ ")"
-    | Tuple (li, ri), Bool b ->
-      "(" ^ string_of_tuple (Tuple (li, ri))  ^ " , " ^ string_of_bool b ^ ")"
-    | Tuple (l1, r1), Tuple (l2, r2) ->
-      "(" ^ string_of_tuple (Tuple (l1, r1))  ^ 
-      " , " ^ string_of_tuple (Tuple (l2, r2)) ^ ")"
-    | _ -> failwith "Precondition violated: Tuples must contain values inside"
-
 let rec string_of_val e = 
   match e with
   | Int i -> string_of_int i
   | Bool b -> string_of_bool b
-  | Tuple (l, r) as t -> string_of_tuple t
-  | Left v -> "left of " ^ string_of_val v
-  | Right v -> "left of " ^ string_of_val v
-  | Binop _  | IfThenElse _ | Let _ | Fst _ | Snd _-> failwith "Incorrect evaluation"
+  | Tuple (l, r) -> "(" ^ string_of_val l ^ " , " ^ string_of_val r ^ ")"
+  | Left v -> "Left of (" ^ string_of_val v ^ ")"
+  | Right v -> "Right of (" ^ string_of_val v ^ ")"
+  | Fun (var, expr) -> "<SimCaml function>"
+  | Binop _  | IfThenElse _ | Let _ | Fst _ | Snd _ | MatchWith _ | Apply _ -> 
+    failwith "Precondition Violation : Incorrect evaluation"
   | Var s -> failwith ("Unbound variable " ^ s)
 
 let interpret s = 
